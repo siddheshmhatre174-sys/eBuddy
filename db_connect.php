102e2547ff04a0c729c75c31dbf2a1aef4c16d52
siddheshmhatre174-sys <siddheshmhatre174@gmail.com>
2025-08-13 13:01:27 +0530

    Add files via upload

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$db = 'cld';
+$user = 'root';
+$pass = '';
+$port = '3306'; // Use port 3307 as requested
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+
+if ($conn->connect_error) {
+    die('Connection failed: ' . $conn->connect_error);
+}
+?>
